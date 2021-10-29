364827453885bb4433a07856da8ed45de24330b0
Pedro Roseno <pedro.roseno@invillia.com>
2021-10-29 09:50:32 -0300

    Classe programa atualizada

--- src/Programa.java
+++ src/Programa.java
@@ -1,4 +1,5 @@
 import exercicio1.Pessoa;
+import exercicio2.Agenda;
 
 import java.time.LocalDate;
 import java.util.Date;
@@ -6,10 +7,31 @@ import java.util.Locale;
 
 public class Programa {
     public static void main(String[] args) {
+        //Exercício 1
+        /*
         Pessoa pedro = new Pessoa();
         pedro.setNome("Pedro Roseno");
         pedro.setAltura(175);
         pedro.setData(LocalDate.of(2000, 7, 24));
         System.out.println(pedro.calculaIdade());
+         */
+        //Exercício2
+
+        Pessoa pessoa1 = new Pessoa("Pedro Roseno", LocalDate.of(2000, 7, 24), 175);
+        Pessoa pessoa2 = new Pessoa("José", LocalDate.of(1974, 7, 18), 180);
+        Agenda agenda = new Agenda();
+        agenda.armazenaPessoa(pessoa1);
+        agenda.armazenaPessoa(pessoa2);
+        System.out.println(agenda.getAgendaPessoas()[0].getNome());
+        System.out.println(agenda.getAgendaPessoas()[1].getNome());
+        agenda.removePessoa("Pedro Roseno");
+        System.out.println(agenda.getAgendaPessoas()[0]);
+        System.out.println(agenda.buscaPessoa("José"));
+        agenda.armazenaPessoa(pessoa1);
+        System.out.println(agenda.imprimeAgenda());
+        System.out.println(agenda.imprimePessoa(0));
+
+
+
     }
 }
