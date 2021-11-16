f9978a5dff3d1f41c389ea8ff0465b249b57494a
Pedro <phrbs@cin.ufpe.br>
2021-11-16 09:36:14 -0300

    Adicionando instruções no input

--- src/exercicio5/Selecao.java
+++ src/exercicio5/Selecao.java
@@ -9,8 +9,10 @@ public class Selecao extends Controle{
     public void insereCaminhao(){
         String escolhaTipo = "flag";
         while(!escolhaTipo.equals("fim")){
+            System.out.println("Escolha o tipo de caminhão (alfa, beta ou fim para sair): ");
             escolhaTipo = leString();
             if (!escolhaTipo.equals("fim")) {
+                System.out.println("Digite a quantidade de pluviometros a ser transportada: ");
                 int escolhaQuantidade = leInteiro();
                 List<Pluviometro> listaPluviometro = leListaPluviometro(escolhaQuantidade);
                 this.listaCaminhoes.add(new Caminhao(escolhaTipo, escolhaQuantidade, listaPluviometro));
@@ -34,6 +36,7 @@ public class Selecao extends Controle{
     public List<Pluviometro> leListaPluviometro(int escolhaQuantidade) {
         List<Pluviometro> listaPluviometro = new ArrayList<>();
         for(int i=0; i < escolhaQuantidade; i++){
+            System.out.println("Digite o tipo de pluviometro(A, B ou C): ");
             listaPluviometro.add(new Pluviometro(leString().toUpperCase().replaceAll(" ", "")));
         }
         return listaPluviometro;
